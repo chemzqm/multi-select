e2122aaca71bcdf36bd5bfaecf4a0ed4888cc75e
chemzqm <chemzqm@gmail.com>
2013-09-20 01:31:26 +0800

    no check before append value

--- index.js
+++ index.js
@@ -313,8 +313,6 @@ MultiSelect.prototype.max = function(number) {
 }
 
 MultiSelect.prototype.appendValue = function(id) {
-  var vs = this.value().split(',');
-  if (vs.indexOf(id) !== -1) return;
   var li = this.dropdown.find('[data-id="' + id + '"]');
   var text = li.html();
   $('<li class="multiselect-search-choice" data-id="' + id + '"><div>' + text + '</div>'+
